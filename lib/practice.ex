651cc3c8eb3676d6223b676c31443055661c113a
sikang hu <sikang_hu@berkeley.edu>
2019-09-19 11:33:02 -0400

    add factor and palindrome

--- lib/practice.ex
+++ lib/practice.ex
@@ -17,9 +17,10 @@ defmodule Practice do
   end
 
   def factor(x) do
-    # Maybe delegate this too.
-    [1,2,x]
+    Practice.Factor.fac(x)
   end
 
-  # TODO: Add a palindrome? function.
+  def palindrome(x) do
+    Practice.Palidrome.pan(x)
+  end
 end
