830dc514549d3e9234d25690bf23758ee4810534
sikang hu <sikang_hu@berkeley.edu>
2019-09-19 11:26:25 -0400

    modified controller and router

--- lib/practice_web/controllers/page_controller.ex
+++ lib/practice_web/controllers/page_controller.ex
@@ -6,7 +6,7 @@ defmodule PracticeWeb.PageController do
   end
 
   def double(conn, %{"x" => x}) do
-    {x, _} = Integer.parse(x)
+    {x, _} = Float.parse(x)
     y = Practice.double(x)
     render conn, "double.html", x: x, y: y
   end
@@ -21,6 +21,8 @@ defmodule PracticeWeb.PageController do
     render conn, "factor.html", x: x, y: y
   end
 
-  # TODO: Add an action for palindrome.
-  # TODO: Add a template for palindrome over in lib/*_web/templates/page/??.html.eex
+  def palindrome(conn, %{"x" => x}) do
+    y = Practice.palindrome(x)
+    render conn, "palindrome.html", x: x, y: y
+  end
 end
